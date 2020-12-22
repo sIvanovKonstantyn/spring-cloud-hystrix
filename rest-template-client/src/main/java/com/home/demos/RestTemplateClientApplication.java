9eaac32871af609821ffc60493c210a7205231b8
kivanov <svosh2@gmail.com>
2020-12-22 15:15:01 +0200

    Add hystrix dashboard.

--- rest-template-client/src/main/java/com/home/demos/RestTemplateClientApplication.java
+++ rest-template-client/src/main/java/com/home/demos/RestTemplateClientApplication.java
@@ -1,37 +1,30 @@
 package com.home.demos;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDateTime;
-import java.util.UUID;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@RestController
+@EnableHystrixDashboard
 public class RestTemplateClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestTemplateClientApplication.class, args);
     }
 
-    @Bean
-    public String callResult(RestTemplateClient restTemplateClient) throws InterruptedException {
 
-        String result = null;
+    @Autowired
+    private RestTemplateClient restTemplateClient;
 
-        for (int i = 0; i < 10; i++) {
-            result = restTemplateClient.apiMethod(UUID.randomUUID().toString());
-            System.out.println(
-                    String.format(
-                            "%s: %s",
-                            LocalDateTime.now(),
-                            result
-                    )
-            );
-        }
-
-        return result;
+    @GetMapping("/api/{id}")
+    public String apiMethod(@PathVariable("id") String id) {
+        return restTemplateClient.apiMethod(id);
     }
 }
